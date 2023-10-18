d7f0dbcfaf1901094af94bb352501ce4afe34c4f
Nicholas Jovianto <nicholas.jovianto@gmail.com>
2023-10-19 01:10:28 +0300

    second

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 
 const handleRequest = (request) => {
-  return new Response("Seeking truths beyond the meaning of life, you will find 43.");
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
 serve(handleRequest, { port: 7777 });
